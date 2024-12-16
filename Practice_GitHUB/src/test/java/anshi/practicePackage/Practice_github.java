61b4ac535545c85ff1a3df41bec64452e031cd45
anshi <anshi@DESKTOP-NB60TP2>
2024-12-16 14:47:07 +0530

    Added new package, anshi.practicePackage!!!

---
+++ Practice_GitHUB/src/test/java/anshi/practicePackage/Practice_github.java
@@ -0,0 +1,12 @@
+package anshi.practicePackage;
+
+import org.testng.annotations.Test;
+
+public class Practice_github {
+	
+	@Test
+	public void practice_github() {
+		System.out.println("test!!!!");
+	}
+
+}
