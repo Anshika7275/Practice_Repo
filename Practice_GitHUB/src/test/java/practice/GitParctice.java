ec2b5c700b0e38762068425cb596b01b6ea385b6
admin <admin@Veena-Manjunath.mshome.net>
2024-12-16 15:49:44 +0530

    changes in practise git class.

--- Practice_GitHUB/src/test/java/practice/GitParctice.java
+++ Practice_GitHUB/src/test/java/practice/GitParctice.java
@@ -5,7 +5,8 @@ public class GitParctice {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hi bye!!!!!!!");
-		System.out.println("Hi sana...!!!!!!!!!!");
+		System.out.println("Hi sana..dfhf.!!!!!!!!!!");
+		
 
 	}
 
