d4d0228d552426346fc73b48b731c1682c5f48de
Debasnata Rath <Debasnata Rath@LAPTOP-RM4DVEEJ>
2024-12-16 13:36:33 +0530

    this code contains the sopln statement

---
+++ Practice_GitHUB/src/test/java/practice/Debasnata.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Debasnata {
+	@Test
+	public void exact_Name()
+	{
+		System.out.println(" I LOVE MY INDIA");
+		System.out.println("git colaboration practice");
+	}
+
+}
