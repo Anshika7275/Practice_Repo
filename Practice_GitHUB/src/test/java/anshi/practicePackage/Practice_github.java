272d3ad61e4b29e6e18eeb282682ff9b363f765d
anshi <anshi@DESKTOP-NB60TP2>
2024-12-16 15:45:16 +0530

    updated

--- Practice_GitHUB/src/test/java/anshi/practicePackage/Practice_github.java
+++ Practice_GitHUB/src/test/java/anshi/practicePackage/Practice_github.java
@@ -7,13 +7,7 @@ public class Practice_github {
 	@Test
 	public void practice_github() {
 		System.out.println("test!!!!");
-		System.out.println("test!!!!");
-		System.out.println("test!!!!");
-		System.out.println("test!!!!");
-		System.out.println("rtyuio");
-		System.out.println("rtyuio");
-		System.out.println("rtyuio");
-
+		
 	}
 
 }
